a4d3f5dd70d0a02f59c71b6b5c576667ff10ccc5
Jose Rodríguez <antonio.rodriguez@fonix.com.mx>
2018-12-18 13:15:53 -0600

    definiendo tienda(store) de datos

--- contador-vuex/src/store.js
+++ contador-vuex/src/store.js
@@ -4,13 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        counter: 0
+    },
+    mutations: {
+        increment(state) {
+            state.counter += 1;
+        },
+        decrement(state) {
+            state.counter -= 1;
+        }
+    },
 })
