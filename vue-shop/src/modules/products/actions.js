99eb7c5dbd2c2b5e2315803aa9b577d4475b2f1b
Jose Rodríguez <antonio.rodriguez@fonix.com.mx>
2018-12-19 14:48:15 -0600

    logica del listado de productos

--- vue-shop/src/modules/products/actions.js
+++ vue-shop/src/modules/products/actions.js
@@ -1,5 +1,5 @@
 export async function fetchProducts({ commit }) {
     const data = await fetch('/fixtures/products.json')
-    const products = data.json()
+    const products = await data.json()
     commit('products/setProducts', products, { root: true })
 }
