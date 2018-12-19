102453085f1a40115f9cdbe8473ff13b4b2163eb
Jose Rodríguez <antonio.rodriguez@fonix.com.mx>
2018-12-19 11:45:58 -0600

    instalando VuePaginate, BootstrapVue y configurarlos como plugins

---
+++ vue-shop/src/plugins/boostrap-vue.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import BootsrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+
+Vue.use(BootsrapVue)
